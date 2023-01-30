098c557096dddd4270c5d2b131c91e7a22e62e40
Maryia <maritsydik@gmail.com>
2023-01-30 17:06:06 +0100

    php

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ window.addEventListener('DOMContentLoaded', function() {
     // modal
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal'),
-        modalCloseBtn = document.querySelector('[data-close]');
+        modal = document.querySelector('.modal');
 
 
      modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modal.classList.add('show');
-            modal.classList.remove('hide');
-            document.body.style.overflow = 'hidden';
-        });
+        btn.addEventListener('click', openModal);
     });
+
+    function openModal(){
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+    }
 
     function closeModal() {
         modal.classList.add('hide');
@@ -23,11 +24,9 @@ window.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = '';
         
     }
-    
-    modalCloseBtn.addEventListener('click', closeModal);
 
     modal.addEventListener('click', (e) => {
-        if (e.target === modal) {
+        if (e.target === modal || e.target.getAttribute('data-close') == "") {
             closeModal();
         }
     });
@@ -74,5 +73,90 @@ window.addEventListener('DOMContentLoaded', function() {
             });
 		}
     });
+
+
+
+
+
+
+
+    
+
+    // FORMS
+
+    
+    const forms = document.querySelectorAll('form');
+    const message = {
+        loading: './img/spinner.svg',
+        success: 'Спасибо! Скоро с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
+            form.insertAdjacentElement('afterend', statusMessage);
+        
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
+
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+            });
+        });
+    }
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.classList.add('hide');
+        openModal();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>×</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal();
+        }, 4000);
+    }
+
+
 
 });
---
+++ server.php
@@ -0,0 +1,52 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
+
+
+    $mail = new PHPMailer(true);
+    $mail->charset = 'UTF-8";
+    $mail->setLanguage('ru', 'phpmailer/language/');
+    $mail->isHTML(true);  
+
+
+
+
+    $mail->addAddress('maritsydik@gmail.com'); 
+    $mail->Subject = 'Запись на консультацию;
+
+    $body = '<h1>Запись</h1>';
+
+    if(trim(!empty($_POST['name']))){
+        $body .='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+    }
+    if(trim(!empty($_POST['from']))){
+        $body .='<p><strong>Откуда вы:</strong> '.$_POST['from'].'</p>';
+    }
+    if(trim(!empty($_POST['contacts']))){
+        $body .='<p><strong>Контакты:</strong> '.$_POST['contacts'].'</p>';
+    }
+    if(trim(!empty($_POST['user_profile']))){
+        $body .='<p><strong>Время:</strong> '.$_POST['user_profile'].'</p>';
+    }
+    if(trim(!empty($_POST['request']))){
+        $body .='<p><strong>Запрос:</strong> '.$_POST['request'].'</p>';
+    }
+
+    $mail->Body = $body;
+  
+
+    
+?>
+
+
+
+
+
+
+
+
+
